d64d5c7ebd3fa1dd321b9ee7ce65b1a018436b0d
RETR0-OS <aadityajindal12@gmail.com>
2025-02-11 01:45:23 -0700

    Adding script to detect key-point coordinates on live-streamed webcame footage using ViTPose

--- AI/Welcome.md
+++ AI/Welcome.md
@@ -8,6 +8,6 @@ The Dev tests directory contains test code to test isolated features of the appl
 ### stream_from_webcam.py
 This file contains test code to stream the video from the webcam of a device. The application may or may not use the code, depending upon whether video streaming is handled in python or JavaScript.
 ### yolo_human_detection.py
-This scripts opens the devices webcam an streams the footage. The streamed footage is them passed through the YOLOv8 algorithm to detect the bounding box for the primary person in the image. The footage is then cropped to the bounding box containing the primary person. The cropped image will later be passed to ViTPose for Key-point detection.
+This script opens the device's webcam and streams the footage. The streamed footage is then passed through the YOLOv8 algorithm to detect the bounding box for the primary person in the image. The footage is then cropped to the bounding box containing the primary person. The cropped image will later be passed to ViTPose for Key-point detection.
 ### yolov8s.pt
 The pre-trained YOLOv8 weights by Ultralytics downloaded by the ultralytics library required to run inferences.
---
+++ AI/dev_tests/pose_estimation.py
@@ -0,0 +1,77 @@
+import cv2
+from ultralytics import YOLO
+from PIL import Image
+import numpy as np
+from transformers import AutoProcessor, VitPoseForPoseEstimation
+import torch
+
+'''
+    This script opens the device's webcam and streams the footage.
+    The streamed footage is then passed through the YOLOv8 algorithm to detect the bounding box for the primary person in the image.
+    The footage is then cropped to the bounding box containing the primary person.
+    The cropped image will later be passed to ViTPose for Key-point detection.
+'''
+
+
+def get_box_area(box):
+    """
+        This function returns the area of the bounding box bounding an object.
+        :param box: The bounding box for the object in the frame
+        :return: float representing the area of the bounding box
+    """
+    coordinates = box.xyxy[0].cpu().numpy().astype(int)
+    width = coordinates[2] - coordinates[0]
+    height = coordinates[3] - coordinates[1]
+    return width * height
+
+yolo = YOLO('yolov8s.pt')
+# print(yolo.names)
+
+image_processor = AutoProcessor.from_pretrained("usyd-community/vitpose-base-simple")
+model = VitPoseForPoseEstimation.from_pretrained("usyd-community/vitpose-base-simple", device_map="cpu")
+
+
+vCapture = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = vCapture.read() #Capture webcam footage
+    if not ret:
+        print("Error reading frames") ##Error checking for webcam faults
+        break
+    frame = cv2.resize(frame, (256, 192))
+    results = yolo.track(frame, stream=True, verbose=False) #Make object detection inferences
+
+    for result in results:
+        person_boxes = [x for x in result.boxes if bool(x.cls==0) and bool(float(x.conf) > 0.8)] #Get all people detected in the image with high confidence
+        if person_boxes:
+            main_box = []
+            main_person = max(person_boxes, key=get_box_area) #Gets the person with the largest bounding box, as this is most probably the main person
+            coordinates = main_person.xyxy[0].cpu().numpy().astype(int) #Gets the coordinates for the bounding box for the person
+            main_box.append(coordinates)
+            rgb_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) #Convert to RGB color format
+            pil_image = Image.fromarray(rgb_image) #Convert to PIL Image for processing
+            # pil_image = pil_image.crop((coordinates[0], coordinates[1], coordinates[2], coordinates[3])).convert("RGB") #Crop the image to be closest to the focus person
+            # cv2_image = np.array(pil_image) #Convert back to CV2 image
+            # frame = cv2_image[:, :, ::-1].copy() #Convert to BGR format as this is the one used by openCV
+
+            #frame now contains the cropped image of the main person.
+            main_box = np.array(main_box)
+            main_box[:,2] = main_box[:,2] - main_box[:,0]
+            main_box[:,3] = main_box[:,3] - main_box[:,1]
+            inputs = image_processor(pil_image, boxes=[main_box], return_tensors="pt")
+
+            with torch.no_grad():
+                outputs = model(**inputs)
+            pose_results = image_processor.post_process_pose_estimation(outputs, boxes=[main_box])
+            image_pose_result = pose_results[0][0]["keypoints"]# Retrieves the keypoint coordinates for the main person in the image.
+
+            #fixme: Add code to draw the keypoints on the image
+
+            #fixme: Add code to upscale the image to the original size for display.
+
+    cv2.imshow('frame', frame) #Display the re-rendered frame
+
+    if cv2.waitKey(1) == ord('q'): #stop capturing if 'q' is clicked
+        vCapture.release()
+        cv2.destroyAllWindows()
+        break
