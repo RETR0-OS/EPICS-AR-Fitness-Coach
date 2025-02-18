bd174e091f2b4fbcc0b21d119b031a39bf1f8511
RETR0-OS <aadityajindal12@gmail.com>
2025-02-17 19:01:46 -0700

    update line and point weights

--- AI/dev_tests/pose_estimation.py
+++ AI/dev_tests/pose_estimation.py
@@ -5,12 +5,12 @@ import numpy as np
 from transformers import AutoProcessor, VitPoseForPoseEstimation
 import torch
 
-'''
+"""
     This script opens the device's webcam and streams the footage.
     The streamed footage is then passed through the YOLOv8 algorithm to detect the bounding box for the primary person in the image.
     The footage is then cropped to the bounding box containing the primary person.
     The cropped image will later be passed to ViTPose for Key-point detection.
-'''
+"""
 
 part_mapper = ["nose", "left_eye", "right_eye", "left_ear", "right_ear", "left_shoulder", "right_shoulder", "left_elbow", "right_elbow", "left_wrist", "right_wrist", "left_hip", "right_hip", "left_knee", "right_knee", "left_ankle", "right_ankle"]
 
@@ -39,51 +39,51 @@ def draw_keypoints(image, results):
     for i in range(len(scores)):
         if scores[i] > 0.75:
             available_keypoints[part_mapper[i]] = keypoints[i] #Add keypoint to available keypoints.
-            cv2.circle(image, (int(keypoints[i][0]), int(keypoints[i][1])), 5, (0, 255, 0), -1) #Draw point
+            cv2.circle(image, (int(keypoints[i][0]), int(keypoints[i][1])), 2, (0, 255, 0), -1) #Draw point
     keys = list(available_keypoints.keys())
 
     #Draw skeleton
     #fixme: See if this can be done in a more efficient way
     if "nose" in keys:
         if "left_eye" in keys:
-            cv2.line(image, (int(available_keypoints["nose"][0]), int(available_keypoints["nose"][1])), (int(available_keypoints["left_eye"][0]), int(available_keypoints["left_eye"][1])), (0, 255, 0), 2)
+            cv2.line(image, (int(available_keypoints["nose"][0]), int(available_keypoints["nose"][1])), (int(available_keypoints["left_eye"][0]), int(available_keypoints["left_eye"][1])), (0, 255, 0), 1)
         if "right_eye" in keys:
-            cv2.line(image, (int(available_keypoints["nose"][0]), int(available_keypoints["nose"][1])), (int(available_keypoints["right_eye"][0]), int(available_keypoints["right_eye"][1])), (0, 255, 0), 2)
+            cv2.line(image, (int(available_keypoints["nose"][0]), int(available_keypoints["nose"][1])), (int(available_keypoints["right_eye"][0]), int(available_keypoints["right_eye"][1])), (0, 255, 0), 1)
         if "left_shoulder" in keys:
-            cv2.line(image, (int(available_keypoints["nose"][0]), int(available_keypoints["nose"][1])), (int(available_keypoints["left_shoulder"][0]), int(available_keypoints["left_shoulder"][1])), (0, 255, 0), 2)
+            cv2.line(image, (int(available_keypoints["nose"][0]), int(available_keypoints["nose"][1])), (int(available_keypoints["left_shoulder"][0]), int(available_keypoints["left_shoulder"][1])), (0, 255, 0), 1)
         if "right_shoulder" in keys:
-            cv2.line(image, (int(available_keypoints["nose"][0]), int(available_keypoints["nose"][1])), (int(available_keypoints["right_shoulder"][0]), int(available_keypoints["right_shoulder"][1])), (0, 255, 0), 2)
+            cv2.line(image, (int(available_keypoints["nose"][0]), int(available_keypoints["nose"][1])), (int(available_keypoints["right_shoulder"][0]), int(available_keypoints["right_shoulder"][1])), (0, 255, 0), 1)
     if "left_eye" in keys and "left_ear" in keys:
-        cv2.line(image, (int(available_keypoints["left_eye"][0]), int(available_keypoints["left_eye"][1])), (int(available_keypoints["left_ear"][0]), int(available_keypoints["left_ear"][1])), (0, 255, 0), 2)
+        cv2.line(image, (int(available_keypoints["left_eye"][0]), int(available_keypoints["left_eye"][1])), (int(available_keypoints["left_ear"][0]), int(available_keypoints["left_ear"][1])), (0, 255, 0), 1)
     if "right_eye" in keys and "right_ear" in keys:
-        cv2.line(image, (int(available_keypoints["right_eye"][0]), int(available_keypoints["right_eye"][1])), (int(available_keypoints["right_ear"][0]), int(available_keypoints["right_ear"][1])), (0, 255, 0), 2)
+        cv2.line(image, (int(available_keypoints["right_eye"][0]), int(available_keypoints["right_eye"][1])), (int(available_keypoints["right_ear"][0]), int(available_keypoints["right_ear"][1])), (0, 255, 0), 1)
     if "left_shoulder" in keys and "left_elbow" in keys:
         if "left_elbow" in keys:
-            cv2.line(image, (int(available_keypoints["left_shoulder"][0]), int(available_keypoints["left_shoulder"][1])), (int(available_keypoints["left_elbow"][0]), int(available_keypoints["left_elbow"][1])), (0, 255, 0), 2)
+            cv2.line(image, (int(available_keypoints["left_shoulder"][0]), int(available_keypoints["left_shoulder"][1])), (int(available_keypoints["left_elbow"][0]), int(available_keypoints["left_elbow"][1])), (0, 255, 0), 1)
         if "right_shoulder" in keys:
-            cv2.line(image, (int(available_keypoints["left_shoulder"][0]), int(available_keypoints["left_shoulder"][1])), (int(available_keypoints["right_shoulder"][0]), int(available_keypoints["right_shoulder"][1])), (0, 255, 0), 2)
+            cv2.line(image, (int(available_keypoints["left_shoulder"][0]), int(available_keypoints["left_shoulder"][1])), (int(available_keypoints["right_shoulder"][0]), int(available_keypoints["right_shoulder"][1])), (0, 255, 0), 1)
         if "left_hip" in keys:
-            cv2.line(image, (int(available_keypoints["left_shoulder"][0]), int(available_keypoints["left_shoulder"][1])), (int(available_keypoints["left_hip"][0]), int(available_keypoints["left_hip"][1])), (0, 255, 0), 2)
+            cv2.line(image, (int(available_keypoints["left_shoulder"][0]), int(available_keypoints["left_shoulder"][1])), (int(available_keypoints["left_hip"][0]), int(available_keypoints["left_hip"][1])), (0, 255, 0), 1)
     if "right_shoulder" in keys:
         if "right_elbow" in keys:
-            cv2.line(image, (int(available_keypoints["right_shoulder"][0]), int(available_keypoints["right_shoulder"][1])), (int(available_keypoints["right_elbow"][0]), int(available_keypoints["right_elbow"][1])), (0, 255, 0), 2)
+            cv2.line(image, (int(available_keypoints["right_shoulder"][0]), int(available_keypoints["right_shoulder"][1])), (int(available_keypoints["right_elbow"][0]), int(available_keypoints["right_elbow"][1])), (0, 255, 0), 1)
         if "right_hip" in keys:
-            cv2.line(image, (int(available_keypoints["right_shoulder"][0]), int(available_keypoints["right_shoulder"][1])), (int(available_keypoints["right_hip"][0]), int(available_keypoints["right_hip"][1])), (0, 255, 0), 2)
+            cv2.line(image, (int(available_keypoints["right_shoulder"][0]), int(available_keypoints["right_shoulder"][1])), (int(available_keypoints["right_hip"][0]), int(available_keypoints["right_hip"][1])), (0, 255, 0), 1)
     if "left_elbow" in keys and "left_wrist" in keys:
-        cv2.line(image, (int(available_keypoints["left_elbow"][0]), int(available_keypoints["left_elbow"][1])), (int(available_keypoints["left_wrist"][0]), int(available_keypoints["left_wrist"][1])), (0, 255, 0), 2)
+        cv2.line(image, (int(available_keypoints["left_elbow"][0]), int(available_keypoints["left_elbow"][1])), (int(available_keypoints["left_wrist"][0]), int(available_keypoints["left_wrist"][1])), (0, 255, 0), 1)
     if "right_elbow" in keys and "right_wrist" in keys:
-        cv2.line(image, (int(available_keypoints["right_elbow"][0]), int(available_keypoints["right_elbow"][1])), (int(available_keypoints["right_wrist"][0]), int(available_keypoints["right_wrist"][1])), (0, 255, 0), 2)
+        cv2.line(image, (int(available_keypoints["right_elbow"][0]), int(available_keypoints["right_elbow"][1])), (int(available_keypoints["right_wrist"][0]), int(available_keypoints["right_wrist"][1])), (0, 255, 0), 1)
     if "left_hip" in keys:
         if "left_knee" in keys:
-            cv2.line(image, (int(available_keypoints["left_hip"][0]), int(available_keypoints["left_hip"][1])), (int(available_keypoints["left_knee"][0]), int(available_keypoints["left_knee"][1])), (0, 255, 0), 2)
+            cv2.line(image, (int(available_keypoints["left_hip"][0]), int(available_keypoints["left_hip"][1])), (int(available_keypoints["left_knee"][0]), int(available_keypoints["left_knee"][1])), (0, 255, 0), 1)
         if "right_hip" in keys:
-            cv2.line(image, (int(available_keypoints["left_hip"][0]), int(available_keypoints["left_hip"][1])), (int(available_keypoints["right_hip"][0]), int(available_keypoints["right_hip"][1])), (0, 255, 0), 2)
+            cv2.line(image, (int(available_keypoints["left_hip"][0]), int(available_keypoints["left_hip"][1])), (int(available_keypoints["right_hip"][0]), int(available_keypoints["right_hip"][1])), (0, 255, 0), 1)
     if "right_hip" in keys and "right_knee" in keys:
-        cv2.line(image, (int(available_keypoints["right_hip"][0]), int(available_keypoints["right_hip"][1])), (int(available_keypoints["right_knee"][0]), int(available_keypoints["right_knee"][1])), (0, 255, 0), 2)
+        cv2.line(image, (int(available_keypoints["right_hip"][0]), int(available_keypoints["right_hip"][1])), (int(available_keypoints["right_knee"][0]), int(available_keypoints["right_knee"][1])), (0, 255, 0), 1)
     if "left_knee" in keys and "left_ankle" in keys:
-        cv2.line(image, (int(available_keypoints["left_knee"][0]), int(available_keypoints["left_knee"][1])), (int(available_keypoints["left_ankle"][0]), int(available_keypoints["left_ankle"][1])), (0, 255, 0), 2)
+        cv2.line(image, (int(available_keypoints["left_knee"][0]), int(available_keypoints["left_knee"][1])), (int(available_keypoints["left_ankle"][0]), int(available_keypoints["left_ankle"][1])), (0, 255, 0), 1)
     if "right_knee" in keys and "right_ankle" in keys:
-        cv2.line(image, (int(available_keypoints["right_knee"][0]), int(available_keypoints["right_knee"][1])), (int(available_keypoints["right_ankle"][0]), int(available_keypoints["right_ankle"][1])), (0, 255, 0), 2)
+        cv2.line(image, (int(available_keypoints["right_knee"][0]), int(available_keypoints["right_knee"][1])), (int(available_keypoints["right_ankle"][0]), int(available_keypoints["right_ankle"][1])), (0, 255, 0), 1)
     return image
 
 
@@ -105,7 +105,7 @@ while True:
     results = yolo.track(frame, stream=True, verbose=False) #Make object detection inferences
 
     for result in results:
-        person_boxes = [x for x in result.boxes if bool(x.cls==0) and bool(float(x.conf) > 0.8)] #Get all people detected in the image with high confidence
+        person_boxes = [x for x in result.boxes if bool(x.cls==0) and bool(float(x.conf) > 0.7)] #Get all people detected in the image with high confidence
         if person_boxes:
             main_box = []
             main_person = max(person_boxes, key=get_box_area) #Gets the person with the largest bounding box, as this is most probably the main person
