dd0c9ae3bbe138646c4b9aa352f2bafc020a52cb
Gunbir Singh <gunbirsingh@mac.3500.dhcp.asu.edu>
2025-04-24 18:35:27 -0700

    cleared error

--- Backend/server.js
+++ Backend/server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
-app.use('/api/auth', authRoutes);
+app.use('/api/auth', authRoutes.router);
 app.use('/api/profile', require('./routes/api/profile'));
 
 // Basic route for testing
